df45549ec17356bc23715ecbcf4d4cc6edebf231
Timothy <timothy.chongz.luong@gmail.com>
2016-04-07 16:26:19 -0700

    Testers and points now work for ints and doubles

--- calculator-v2/main.swift
+++ calculator-v2/main.swift
@@ -70,6 +70,24 @@ func subtractTuples(p1: (Int, Int), p2: (Int, Int)) -> (Int, Int) {
     return (p1.0 - p2.0, p1.1 - p2.1)
 }
 
+// Integer versions for dictionary
+func addPoints(p1: [String: Int], p2: [String: Int]) -> (Int, Int) {
+    if p1["x"] == nil || p1["y"] == nil || p2["x"] == nil || p2["y"] == nil {
+        print("Invalid Parameters. Missing a coordinate value.")
+        exit(1)
+    }
+    return (p1["x"]! + p2["x"]!, p1["y"]! + p2["y"]!)
+}
+
+func subtractPoints(p1: [String: Int], p2: [String: Int]) -> (Int, Int) {
+    if p1["x"] == nil || p1["y"] == nil || p2["x"] == nil || p2["y"] == nil {
+        print("Invalid Parameters. Missing a coordinate value.")
+        exit(1)
+    }
+    return (p1["x"]! - p2["x"]!, p1["y"]! - p2["y"]!)
+}
+
+// Double versions for dictionary
 func addPoints(p1: [String: Double], p2: [String: Double]) -> (Double, Double) {
     if p1["x"] == nil || p1["y"] == nil || p2["x"] == nil || p2["y"] == nil {
         print("Invalid Parameters. Missing a coordinate value.")
@@ -85,3 +103,44 @@ func subtractPoints(p1: [String: Double], p2: [String: Double]) -> (Double, Doub
     }
     return (p1["x"]! - p2["x"]!, p1["y"]! - p2["y"]!)
 }
+
+// Moderate Unit Testing (although I suppose I'm not covering alot of cases)
+
+// Calculator
+print("Basic Calc Tests")
+print(add(132, n2: 243))
+print(subtract(132, n2: 243))
+print(multiply(78, n2: 13))
+print(divide(78, n2: 13))
+print(mathOp(132, n2: 243, op: add))
+print(mathOp(132, n2: 243, op: subtract))
+print(mathOp(78, n2: 13, op: multiply))
+print(mathOp(78, n2: 13, op: divide))
+
+// Array fun
+let testArr = [1, 3, 5, 7, 9]
+print("Basic Array Tests")
+print(addArr(testArr))
+print(multArr(testArr))
+print(count(testArr))
+print(avg(testArr))
+print(mathArrOp(testArr, op: addArr))
+print(mathArrOp(testArr, op: multArr))
+print(mathArrOp(testArr, op: count))
+print(mathArrOp(testArr, op: avg))
+
+// Points
+let t1 = (1, 2)
+let t2 = (3, 4)
+print("Tuples")
+print(addTuples(t1, p2: t2))
+print(subtractTuples(t1, p2:t2))
+print("Points")
+let point1 = ["x" : 1, "y": 2]
+let point2 = ["x" : 3, "y": 4]
+let point3 = ["x" : 1.0, "y": 2.0]
+let point4 = ["x" : 3.0, "y": 4.0]
+print(addPoints(point1, p2: point2))
+print(addPoints(point3, p2: point4))
+print(subtractPoints(point1, p2: point2))
+print(subtractPoints(point3, p2: point4))
